3dac5071ac41ed37f8fc4564d6cac63fd3d0314b
mon.nagata <mon_nagata@hotmail.com>
2021-05-08 13:17:34 +0000

    delete: apiフォルダがあった

--- components/MarkdownComponents.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import classes from '../styles/MarkdownComponents.module.scss';
-
-export const ArticleImg: React.FC<React.ImgHTMLAttributes<HTMLImageElement>> = (props) => {
-  return (
-    <img {...props} className={classes.articleImg} />
-  );
-};
--- utils/index.ts
+++
@@ -1,9 +0,0 @@
-export const isEmpty = (x: any, mode?: {
-    str: boolean
-}) => {
-  if (mode?.str) {
-    return x === '';
-  }
-  return x === undefined || x === null;
-};
-export const cls = (...classNames: string[]) => classNames.filter((x) => !isEmpty(x)).join(' ');
